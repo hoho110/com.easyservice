c39039e82d0ed3337708c199ecae421ea3bea8f5
wmsdu <wmsdu@aliyun.com>
2014-08-15 14:40:35 +0800

    增加校验权限

--- src/test/java/com/easyserivce/Test.java
+++ src/test/java/com/easyserivce/Test.java
@@ -1,5 +1,19 @@
 package com.easyserivce;
 
-public class Test {
+import org.junit.Before;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.easyservice.test.IUserService;
+
+public class Test {
+	ApplicationContext context = null;
+	IUserService userService = null;
+
+	@Before
+	public void initContext() {
+		this.context = new FileSystemXmlApplicationContext(
+				"/src/main/webapp/WEB-INF/spring/root-context.xml");
+		this.userService = (IUserService) context.getBean("lenderDao");
+	}
 }
